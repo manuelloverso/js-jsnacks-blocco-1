3f6d338be8383e189ecb75ff8e55ad7ec2ec68c1
manuelloverso <manuelloverso@gmail.com>
2024-02-26 11:44:38 +0100

    add while loop

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,11 +1,24 @@
 let numbers = [];
 
-for (let i = 0; i < 6; i++) {
+/* for (let i = 0; i < 6; i++) {
   let number = Number(prompt("inserisci un numero"));
 
   if (number % 2 != 0) {
     numbers.push(number);
   }
+}
+
+console.log(numbers); */
+
+let i = 0;
+while (i < 6) {
+  let number = Number(prompt("inserisci un numero"));
+
+  if (number % 2 != 0) {
+    numbers.push(number);
+  }
+
+  i++;
 }
 
 console.log(numbers);
